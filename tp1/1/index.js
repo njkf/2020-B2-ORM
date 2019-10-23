6f4c4f98e18949e8a27e4e0d2da7c846e1e6dc81
njkf <43401909+njkf@users.noreply.github.com>
2019-10-23 15:47:31 +0200

    Add files via upload

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -8,6 +8,38 @@ exports = module.exports = {
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
 
+    let place = input[0]
+
+    console.log(1,place);
+
+    for (let i = 1; i < input.length; i++) {
+      if (input.indexOf(i) % 2 == 0) {
+        place -= input[i];
+        }
+        else {
+        place += input[i];
+        }
+      }
+    // je me rend compte a la fin que les 2 chiffres (depassement et dépassé) sont dans le meme index et que ce sont des strings, ca ne pouvait pas marcher...
+  
+    console.log(2,place);
+
+    function prix(place) {
+      if (place < 100) {
+        return "1000";
+      }
+      else if (place > 100 && place < 10000) { 
+        return "1000";
+      }
+      else {
+        return "KO";
+      }
+    }
+    
+    console.log(3,prix)
+
+
+
     // AND HERE
   },
   verify: function (dataset, output) {
@@ -18,3 +50,4 @@ exports = module.exports = {
     }
   }
 }
+
